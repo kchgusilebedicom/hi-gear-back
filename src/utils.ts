34fc188daa749e51698e68386c2875aad3d10e71
Sw33tPiggy <44779148+Sw33tPiggy@users.noreply.github.com>
2021-11-30 16:14:05 +0300

    Added typogrphy

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,19 @@
+import Typograf from 'typograf';
 export const makePath = (article: number) => {
   let id = 1;
   return () => {
     return `images/articles${article}-img${id++}`;
   };
+};
+
+console.log(Typograf);
+const tp = new Typograf({ locale: ['ru', 'en-US'] });
+
+export const html = (template: Parameters<typeof String.raw>[0]) => {
+  const string = `${template.raw}`;
+
+  // console.log(string);
+  const out = tp.execute(string);
+  // console.log(out);
+  return out;
 };
